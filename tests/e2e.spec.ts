fb7c95ec3400a46e72e54cf50022719de13e81fc
sventech27 <myupbusiness123@gmail.com>
2025-02-07 17:56:27 +0100

    Use pnpm preview instead of pnpm dev

--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -1,16 +1,16 @@
 import { test, expect } from '@playwright/test';
 
 test('if user not logged in, when click on pricing button then redirect to authentication page', async ({ page }) => {
-  await page.goto('http://127.0.0.1:5173/payment');
+  await page.goto('http://127.0.0.1:4173/payment');
 
   await page.getByRole('button', { name: 'Get Mysaas' }).click();
-  await page.waitForURL('http://127.0.0.1:5173/auth/login');
+  await page.waitForURL('http://127.0.0.1:4173/auth/login');
 
-  await expect(page).toHaveURL('http://127.0.0.1:5173/auth/login');
+  await expect(page).toHaveURL('http://127.0.0.1:4173/auth/login');
 });
 
 test('if error occurs when submit contact form, then show error message', async ({ page }) => {
-  await page.goto('http://127.0.0.1:5173/contact');
+  await page.goto('http://127.0.0.1:4173/contact');
 
   await page.getByLabel('Email').fill('test@test.test');
   await page.getByLabel('Subject').fill('Test');

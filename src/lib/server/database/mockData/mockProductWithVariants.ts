9d7ddb4c1ecd7eca965be6e24e4034761706b825
sventech27 <myupbusiness123@gmail.com>
2025-02-07 16:04:30 +0100

    Mock db for e2e tests

---
+++ src/lib/server/database/mockData/mockProductWithVariants.ts
@@ -0,0 +1,22 @@
+export const mockProductWithVariants = [
+	{
+		id: 1,
+		name: 'Test Product',
+		description: 'Test Product',
+		iconUrl: '',
+		details: '',
+		status: 'active',
+		variants: [
+			{
+				id: 1,
+				name: 'Test Variant',
+				price: 10,
+				currency: 'USD',
+				billingPeriod: 'monthly',
+				status: 'active'
+			}
+		]
+	}
+];
+
+export default mockProductWithVariants;
--- src/lib/server/database/productService.ts
+++ src/lib/server/database/productService.ts
@@ -1,14 +1,25 @@
-import { db } from "./client";
+import { db } from './client';
+import { env } from '$env/dynamic/private';
+import mockProductWithVariants from './mockData/mockProductWithVariants';
 
 export type ProductWithVariants = Awaited<ReturnType<typeof getActiveProductsWithVariants>>;
 
 export async function getActiveProductsWithVariants() {
-    return await db.query.productsTable.findMany({
-        where: (productsTable, { eq }) => (eq(productsTable.status, "active")),
-        with: {
-            variants: {
-                where: (productVariantTable, { eq }) => eq(productVariantTable.status, "active")
-            }
-        }
-    })
+	if (env.USE_MOCK_DATA) {
+		return mockProductWithVariants;
+	}
+
+	try {
+		return await db.query.productsTable.findMany({
+			where: (productsTable, { eq }) => eq(productsTable.status, 'active'),
+			with: {
+				variants: {
+					where: (productVariantTable, { eq }) => eq(productVariantTable.status, 'active')
+				}
+			}
+		});
+	} catch (error) {
+		console.error(error);
+		return [];
+	}
 }
